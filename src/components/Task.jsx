d90111400bfdfe23dfbd3afaeea5a6ce57aa092f
kuksik07 <kuksik007@gmail.com>
2018-07-01 23:37:25 +0300

    when click to 'delete' on a task, only one task has a popup

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Card, Icon, Popup, Button } from 'semantic-ui-react'
 
-const Task = ({ title, description, color }) =>
+const Task = ({ title, description, color, isOpenPopup, onClickDelete }) =>
         <Card color = {color} style = {{width: '250px'}}>
             <Card.Content extra style = {{borderTop: 'none'}}>
-                <Popup trigger={<Icon link name='close' style = {{float: 'right'}} />} on='click' position='top right'>
+                <Popup trigger={<Icon link name='close' style = {{float: 'right'}} onClick = {onClickDelete} />}
+                       on='click'
+                       position='top right'
+                       open = {isOpenPopup}
+                >
                     <Button.Group>
                         <Button positive>Done</Button>
                         <Button>Not done</Button>
--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Card } from 'semantic-ui-react'
 import Task from './Task'
 import tasks from '../tasks.json'
 
-const TasksList = () =>
-        <Card.Group itemsPerRow={3} centered>
-            {tasks.map(task =>
-                <Task
-                    title = {task.title}
-                    description = {task.description}
-                    color = {task.color}
-                />
-            )}
-        </Card.Group>
+class TasksList extends Component {
+  state = {
+    deleteTask: null,
+  }
+
+  render () {
+      return (
+          <Card.Group itemsPerRow={3} centered>
+              {tasks.map((task, index) =>
+                  <Task
+                      title = {task.title}
+                      description = {task.description}
+                      color = {task.color}
+                      isOpenPopup = {this.state.deleteTask === index}
+                      onClickDelete = {this.handleDeleteClick.bind(this, index)}
+                      key = {index}
+                  />
+              )}
+          </Card.Group>
+      )
+  }
+    handleDeleteClick = taskIndex => this.setState({
+        deleteTask: this.state.deleteTask === taskIndex ? null : taskIndex
+    })
+}
 
 export default TasksList
